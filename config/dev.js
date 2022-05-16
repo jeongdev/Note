0995747a836aa53635bc1ba6cef298a27e814af0
JJ <2kkLqBMscTLhYbfs3Jxv>
2022-05-16 17:50:25 +0900

    회원가입, 로그인, 토큰생성, 로그아웃

--- config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://JJ:x786214.@jj.mgxox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser");
 
-const config = require('./config/key')
+const config = require('./server/config/key')
+const {auth} = require("./server/middleware/auth")
+const {User} = require("./server/model/User")
 
-const { User } = require("./model/User")
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 // 분석해서 가져올 수 있게
 // application/x-www-form-urlencoded
@@ -13,36 +19,102 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
-//userNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-mongoose.connect(config.mongoURI, ).then(()=> console.log('mongo finally'))
-    .catch(err=>console.log(err));
+mongoose.connect(config.mongoURI)
+    .then(() => console.log('mongo finally'))
+    .catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World 안녕하세요! 저절로 될까?')
+    res.send('Hello World 안녕하세요!?')
 })
 
+// app.get('/api/hello', (req, res) => {
+//     res.send("안녕하세요~~")
+// })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원가입할떄 필요한 정보들을 client 에서 가져오면
     // 그것들을 데이터베이스에 넣어준다
 
 
     const user = new User(req.body);
+    console.log(user);
 
     user.save((err, userInfo) => {
-        if(err) return res.json({success: false, err})
+        if (err) {
+            console.log(err);
+            return res.json({success: false, err})
+        }
         return res.status(200).json({
             success: true
         })
-
     })
 })
 
 
+// role 0 일반유저  아니면 관리자
+app.get('/api/users/auth', auth, (req, res) => {
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    // 여기까지 미들웨어를 통과해왔다는 얘기는 authentication true
+    //req.user, req,token
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.role,
+        image: req.user.image
+    })
+
 })
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({_id: req.user._id},
+        {token: ""},
+        (err, user) => {
+            if (err) return res.json({success: false, err});
+            return res.status(200).send({
+                success: true
+            })
+        })
+})
+
+app.post('/api/users/login', (req, res) => {
+
+    // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({email: req.body.email}, (err, user) => {
+        console.log('user', user)
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+            // 비밀번호까지 맞다면 토큰을 생성하기
+            user.generateToken((err, user) => {
+
+                if (err) return res.status(400).send(err);
+
+                // 토큰을 저장한다. 어디에? 쿠키, 로컬ㄹ스토리지 여기서는 쿠키
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({loginSuccess: true, userId: user._id})
+
+            })
+
+        })
+    })
+
+
+})
+
